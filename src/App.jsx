76d206946846058f6c13e3c6268beeb68ef57171
prayag <prayagrajwade01@gmail.com>
2024-06-18 16:52:13 +0530

    Add a New Page to change the color of input based on the User Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,18 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/Home";
+import NameInput from "./components/NameInput";
 
 function App() {
-  const [color, setColor] = useState("olive");
+  
 
   return (
-    <div
-      className="w-full h-screen duration-200"
-      style={{ backgroundColor: color }}
-    >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="fixed flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl">
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "red" }}
-            onClick={() => setColor("red")}
-          >
-            Red
-          </button>
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "Green" }}
-            onClick={() => setColor("Green")}
-          >
-            Green
-          </button>
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "Black" }}
-            onClick={() => setColor("Black")}
-          >
-            Black
-          </button>
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "Orange" }}
-            onClick={() => setColor("Orange")}
-          >
-          Orange
-          </button>
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "#64748b" }}
-            onClick={() => setColor("#64748b")}
-          >
-          Slate
-          </button>
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "Violet" }}
-            onClick={() => setColor("Violet")}
-          >
-          Violet
-          </button>
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "#38bdf8" }}
-            onClick={() => setColor("#38bdf8")}
-          >
-          Sky
-          </button>
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "Teal" }}
-            onClick={() => setColor("Teal")}
-          >
-          Teal
-          </button>
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "Lime" }}
-            onClick={() => setColor("Lime")}
-          >
-          Lime
-          </button>
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "#92400e" }}
-            onClick={() => setColor("#92400e")}
-          >
-          Amber
-          </button>
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "Gray" }}
-            onClick={() => setColor("Gray")}
-          >
-            Gray
-          </button>
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element = {<Home/>}></Route>
+        <Route path="/name-input" element={<NameInput/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
